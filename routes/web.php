2eeaf5f2a1670a98b6472774c6577addad70c1a4
Vankad24 <vankad24@mail.ru>
2025-12-12 22:39:09 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataController;
 
@@ -11,3 +14,7 @@ Route::post('/save-data', [DataController::class, 'saveData'])->name('data.save'
 
 // Роут для отображения всех сохраненных данных в таблице (GET-запрос)
 Route::get('/data-table', [DataController::class, 'showTable'])->name('data.table');
+
+Route::apiResource('api/posts', PostController::class);
+Route::apiResource('api/categories', CategoryController::class);
+Route::apiResource('api/tags', TagController::class);
